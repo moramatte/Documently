da9135f14a6d0cd37eb8e318a46755dc1569c13d
moramatte <moramatte@gmail.com>
2012-04-18 21:13:09 +0200

    Added a virtual keyboard to play around with 3d and inter control communication.

---
+++ src/Documently.WpfClient/Shared/FlippingKey.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Documently.WpfClient.Shared {
+    /// <summary>
+    /// Interaction logic for FlippingKey.xaml
+    /// </summary>
+    public partial class FlippingKey : UserControl {
+        public FlippingKey() {
+            InitializeComponent();
+            DataContext = this;
+        }
+       
+        public string Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(FlippingKey), new PropertyMetadata("*"));
+
+    }
+}
